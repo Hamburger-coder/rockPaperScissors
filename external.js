762a182e80143b1412662462cc641245121ae515
hamburger-coder <joesmast@gmail.com>
2022-11-18 17:50:16 -0500

    Made a working prototype and fixed some bugs, i also added a restart button so you don't have to refresh the page

--- external.js
+++ external.js
@@ -1,4 +1,4 @@
-let playerOption = prompt("Pls input rock, paper, or scissors:");
+let playerChoice = prompt("Pls input rock, paper, or scissors:").toLowerCase();
 
 const options = [
   "Rock",
@@ -9,6 +9,40 @@ const options = [
 function getComputerChoice() {
   let computerChoice = options[Math.floor(Math.random()*options.length)];
   console.log(computerChoice);
+  return computerChoice;
 }
 
-getComputerChoice();
+function gameRound(computerChoice) {
+  if ((playerChoice === "rock") && computerChoice === "Scissors"){
+      alert("You win!");
+  } else if ((playerChoice === "paper") && computerChoice === "Rock"){
+      alert("You win!");
+  } else if ((playerChoice === "scissors") && computerChoice === "Paper"){
+      alert("You win!");
+  } else if ((playerChoice === "rock") && computerChoice === "Paper"){
+      alert("You lose :(");
+  } else if ((playerChoice === "paper") && computerChoice === "Scissors") {
+      alert("You lose :(");
+  } else if ((playerChoice === "scissors") && computerChoice === "Rock"){
+      alert("You lose :(");
+  } else if ((playerChoice === "rock") && computerChoice === "Rock"){
+      alert("It's a tie :|");
+  } else if ((playerChoice === "paper") && computerChoice === "Paper"){
+      alert("It's a tie :|");
+  } else if ((playerChoice === "scissors") && computerChoice === "Scissors"){
+      alert("It's a tie :|");
+  }
+    else {
+      alert("Pls insert a valid option.");
+    }
+}
+
+
+const choice = getComputerChoice();
+gameRound(choice);
+
+const restartButton = document.getElementById("restart");
+
+restartButton.addEventListener("click", () => {
+  window.location.reload();
+})
